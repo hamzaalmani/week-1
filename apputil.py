a0f1210d818eb66dd3e152184b099633670639f0
hamzaalmani <142227908+hamzaalmani@users.noreply.github.com>
2026-01-28 00:55:39 +0000

    final commit

--- apputil.py
+++ apputil.py
@@ -1,3 +1,34 @@
 
 
 # add code below ...
+
+def palindrome(word):
+    cleaned = ""
+    for char in word.lower():
+        if char.isalnum():   # keeps letters and numbers only
+            cleaned += char
+    return cleaned == cleaned[::-1]
+
+palindrome("racecar")       
+palindrome("Nurses Run")
+palindrome("Sit on a potato pan, Otis.")
+palindrome("hello")
+
+
+
+def parentheses(sequence):
+    count = 0
+    for i in sequence:
+        if i == "(":
+            count += 1
+        elif i == ")":
+            count -= 1
+        if count < 0:      
+            return False
+    return count == 0
+
+parentheses("((blah)()()())")
+parentheses("(((())blee))")
+parentheses("(()hello((())()))")
+parentheses("((((((())")
+parentheses("()))")
